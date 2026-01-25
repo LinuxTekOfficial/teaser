fa844689173c3053e6361ce824a64cdc8f53f60b
LinuxTek <256422382+LinuxTekOfficial@users.noreply.github.com>
2026-01-25 02:16:53 -0500

    Add footer, privacy policy, tos

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ document.getElementById("signupForm").addEventListener("submit", function (e) {
   e.preventDefault();
 
   const email = document.getElementById("emailInput").value;
+  const hiddenFieldValue = document.getElementById('hiddenField').value;
   const message = document.getElementById("signupMessage");
 
-  console.log("Collected email:", email);
-
-  message.textContent = "Thanks! You'll receive updates soon.";
+  if (hiddenFieldValue) {
+    e.preventDefault();
+    alert("Spam detected!");
+  } else {
+    console.log("Collected email:", email);
+    message.textContent = "Thanks! You'll receive a confirmation mail.";
+  }
 });
